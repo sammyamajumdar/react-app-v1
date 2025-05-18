8d16a92eca68e4a425f84d6406568e1cbd4d94c7
sammyamajumdar <sammya512@gmail.com>
2025-05-18 12:15:32 +0100

    updated profile photo

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -166,7 +166,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/dp.png"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
